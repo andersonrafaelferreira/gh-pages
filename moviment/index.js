5741a471e21ee9a31bb40b12ffd884c78417693c
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2018-08-07 22:08:57 -0300

    alteracoes do cliente

--- moviment/index.js
+++ moviment/index.js
@@ -1,8 +1,20 @@
-$('ul.nav li.dropdown').hover(function () {
+$(document).ready(function () {
 
-    $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(0);
-}, function () {
-    $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut(0);
+    if ($(document).width() > 1024) {
+
+        $('ul.nav li.dropdown').hover(function () {
+
+            $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(0);
+        }, function () {
+            $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut(0);
+        });
+
+        console.log('bigger screen than a mobile mobile');
+
+
+    } else {
+        console.log('mobile does not apply');
+    }
 });
 
 /*
